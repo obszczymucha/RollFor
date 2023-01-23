c46e831d9378840c6d8596dac42fd9bf1e52fddb
Obszczymucha <obszczymucha@gmail.com>
2023-01-23 18:14:52 +1100

    Clean up test runner.

--- test.sh
+++ test.sh
@@ -22,7 +22,13 @@ run_test() {
   dir=$(dirname "$full_file")
   
   pushd "$dir" > /dev/null || return
-  lua "$file" -v -T Spec -m should -o text | awk '{ gsub("^OK$", "\033[1;32m&\033[0m");
+  local params=(-v -T Spec -m should -o text)
+
+  if [[ "$#" -ne 1 ]]; then
+    params=("${@:2}")
+  fi
+
+  lua "$file" "${params[@]}" | awk '{ gsub("^OK$", "\033[1;32m&\033[0m");
                           gsub("Ok$", "\033[1;32m&\033[0m");
                           gsub("^Failed tests:$", "\033[1;31m&\033[0m");
                           gsub("FAIL$", "\033[1;31m&\033[0m");
@@ -38,7 +44,7 @@ run_all_tests() {
   find . -name "*_test.lua" | while read -r file; do {
     echo
     echo "Testing $file..."
-    run_test "$file"
+    run_test "$file" "$@"
 
     if [[ $TEST_FAILED -ne 0 ]]; then
       return
@@ -88,37 +94,18 @@ on_change() {
     run_test "$file"
   else
     run_all_tests
-    #local base
-    #base=$(echo "$file" | sed -E "s|^(.+)\.lua$|\1|g")
-
-    #local dir
-    #dir=$(dirname "$file")
-
-    #local test_filename="${base}_test.lua"
-    #local test_file="test/$test_filename"
-
-    #if [[ -f "$test_file" ]]; then
-    #  printf "${WHITE_COLOR}Changed: ${NO_COLOR}%s.\n" "$file"
-    #  printf "${WHITE_COLOR}Running test: ${NO_COLOR}%s\n" "$test_file"
-    #  run_test "$test_file"
-    #else
-    #  echo "Test file: $test_file"
-    #  echo "Changed: $file. No test found."
-    #fi
   fi
 }
 
 run() {
-  if [[ $# == 0 ]]; then
-    run_all_tests
-  elif [[ $1 == "listen" ]]; then
+  if [[ $1 == "listen" ]]; then
     "$0"
     listen
   elif [[ $1 == "--help" ]]; then
     print_usage
     exit 1
   else
-    on_change "$1"
+    run_all_tests "$@"
   fi
 }
 
