98655394c19bc1ae1f54d3b675bd8abe9160b37c
Obszczymucha <obszczymucha@gmail.com>
2023-09-13 22:46:11 +1000

    Enable and fix trade-related soft-res tests.

--- test/softres_rolls_test.lua
+++ test/softres_rolls_test.lua
@@ -25,10 +25,9 @@ local tick = utils.tick
 local loot = utils.loot
 local item = utils.item
 local award = utils.award
---I had to hack trading with timers in 2.4.3, so I'm not sure if I want to test this now.
---local trade_with = utils.trade_with
---local trade_items = utils.trade_items
---local trade_complete = utils.trade_complete
+local trade_with = utils.trade_with
+local trade_items = utils.trade_items
+local trade_complete = utils.trade_complete
 local master_loot = utils.master_loot
 local confirm_master_looting = utils.confirm_master_looting
 local clear_dropped_items_db = utils.clear_dropped_items_db
@@ -231,35 +230,38 @@ function SoftResIntegrationSpec:should_allow_others_to_roll_if_player_who_soft_r
 end
 
 --Disabling due to fucking 2.4.3 hack with delayed trade check.
---function SoftResIntegrationSpec:should_allow_others_to_roll_if_player_who_soft_ressed_already_received_the_item_via_trade()
----- Given
---master_looter( "Psikutas" )
---is_in_raid( leader( "Psikutas" ), "Obszczymucha", "Ponpon" )
---soft_res( sr( "Obszczymucha", 123 ) )
+function SoftResIntegrationSpec:should_allow_others_to_roll_if_player_who_soft_ressed_already_received_the_item_via_trade()
+  -- Given
+  master_looter( "Psikutas" )
+  is_in_raid( leader( "Psikutas" ), "Obszczymucha", "Ponpon" )
+  soft_res( sr( "Obszczymucha", 123 ) )
 
----- When
---loot( item( "Hearthstone", 123 ), item( "Hearthstone", 123 ) )
---roll_for( "Hearthstone", 1, 123 )
---trade_with( "Obszczymucha" )
---trade_items( nil, { item_link = utils.item_link( "Hearthstone", 123 ), quantity = 1 } )
---trade_complete()
---roll_for( "Hearthstone", 1, 123 )
---roll( "Ponpon", 1 )
---tick( 8 )
+  -- When
+  loot( item( "Hearthstone", 123 ), item( "Hearthstone", 123 ) )
+  roll_for( "Hearthstone", 1, 123 )
+  trade_with( "Obszczymucha" )
+  trade_items( nil, { item_link = utils.item_link( "Hearthstone", 123 ), quantity = 1 } )
+  trade_complete()
+  tick()
+  roll_for( "Hearthstone", 1, 123 )
+  roll( "Ponpon", 1 )
+  repeating_tick( 8 )
 
----- Then
---assert_messages(
---r( "2 items dropped:", "1. [Hearthstone] (SR by Obszczymucha)", "2. [Hearthstone]" ),
---rw( "[Hearthstone] is soft-ressed by Obszczymucha." ),
---c( "RollFor: Started trading with Obszczymucha." ),
---c( "RollFor: Trading with Obszczymucha complete." ),
-----c( "RollFor: Obszczymucha received [Hearthstone]." ),
---rw( "Roll for [Hearthstone]: /roll (MS) or /roll 99 (OS)" ),
---r( "Stopping rolls in 3", "2", "1" ),
---cr( "Ponpon rolled the highest (1) for [Hearthstone]." ),
---rolling_finished()
---)
---end
+  -- Then
+  assert_messages(
+    r( "2 items dropped:", "1. [Hearthstone] (SR by Obszczymucha)", "2. [Hearthstone]" ),
+    rw( "[Hearthstone] is soft-ressed by Obszczymucha." ),
+    c( "RollFor: Started trading with Obszczymucha." ),
+    c( "RollFor: Giving in slot 1: 1x[Hearthstone]" ),
+    c( "RollFor: Trading with Obszczymucha complete." ),
+    c( "RollFor: Traded: 1x[Hearthstone]" ),
+    c( "RollFor: Obszczymucha received [Hearthstone]." ),
+    rw( "Roll for [Hearthstone]: /roll (MS) or /roll 99 (OS)" ),
+    r( "Stopping rolls in 3", "2", "1" ),
+    cr( "Ponpon rolled the highest (1) for [Hearthstone]." ),
+    rolling_finished()
+  )
+end
 
 function SoftResIntegrationSpec:should_allow_others_to_roll_if_player_who_soft_ressed_already_received_the_item_via_master_loot()
   -- Given
